35fae033b5e31b8692b3cc22725af574d196038e
janae405 <157657918+janae405@users.noreply.github.com>
2024-02-09 22:05:26 -0800

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,12 +1,19 @@
 import random
-num = random.randint(1, 100)
-
-user = input("Enter your guess: ")
-user = int(user)
-
-if user > num:
-    print("Too high, it was", num)
-elif user < num:
-    print("Too low, it was", num)
-else:
-    print("You got it!")
+guesses=0
+num= random.randint(1,100)
+user = 0
+counter = 1
+lowRange = (1)
+highRange =(100)
+while user!= num:
+    user = int(input(f"Guess a number between {lowRange}  and {highRange}: "))
+    if user >num:
+        highRange=user
+        user = print ("Too large.")
+        guesses=guesses+1
+    elif user<num:
+         lowRange=user
+         user = print("Too small.")
+         guesses=guesses+1
+    else:
+          print(f"You got it. It only took, {guesses}, to get it correct")
